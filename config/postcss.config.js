18dac2245664998304f986f0b3762775dedd8cc8
sean-pwd <sean@powerfulwebdesign.com.au>
2021-11-12 00:04:17 +1100

    fix: postcss conditional, refactor code

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,10 +1,13 @@
 const autoprefixer = require('autoprefixer');
-const purgecss = require('@fullhuman/postcss-purgecss');
-const whitelister = require('purgecss-whitelister');
+// const purgecss = require('@fullhuman/postcss-purgecss');
+// const whitelister = require('purgecss-whitelister');
 
 module.exports = {
   plugins: [
-    autoprefixer(),
+    // the following runs when env is NOT development
+    !process.env.HUGO_ENVIRONMENT === 'development'
+    ? autoprefixer()
+    : null,
     // purgecss({
     //   content: [
     //     './layouts/**/*.html',
